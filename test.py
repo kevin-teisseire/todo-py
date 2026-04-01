4877f05b1cf49b4508bce42fd7fd4c361cd8f555
Kevin Teisseire <teisseire.kevin@gmail.com>
2026-04-01 12:19:30 +0200

    Removed tasks.json and tests.py from tracking

--- test.py
+++
@@ -1,8 +0,0 @@
-datas = [{"id": 1, "title": "danser", "priority": "low", "status": "done"}, {"id": 2, "title": "Ranger salon", "priority": "low", "status": "on going"}, {"id": "3", "title": "Ranger salon", "priority": "mid", "status": "on going"}, {"id": 4, "title": "Ranger salon", "priority": "low", "status": "on going"}, {"id": "5", "title": "Ranger salon", "priority": "high", "status": "on going"}, {"id": 6, "title": "Ranger salon", "priority": "low", "status": "on going"}]
-
-# print(sorted(datas, key=lambda x: x['priority'] == 'low'))
-
-print(sum(1 for el in datas if el['priority'] == 'low'))
-
-# li = [1, 2, 3, 4, 4, 4]
-# print(li.count(4))
